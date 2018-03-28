6057bcfd5aca6b5f7db7e625ee19e03500689104
조윤식 <yoonsik.cho@sundaytoz.com>
2018-03-28 13:58:35 +0900

    config 클래스 추가

---
+++ CrossIt/WebContent/js/GameConfig.js
@@ -0,0 +1,15 @@
+var CLIENT_VERSION = "0.0.0";
+
+var EColor = {
+		RED : 0,
+		YELLOW : 1,
+		GREEN : 2,
+		BLUE : 3,
+		PURPLE : 4,
+		GRAY : 5
+	};
+
+var EShapeType = {
+	CIRCLE : 0,
+	RECT : 1
+};
